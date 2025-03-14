58cd41b13be1bca3e81d167cfd73e2d0fae49751
aliviaaaa <lil.alivia23@gmail.com>
2025-03-14 13:51:17 +0700

    refactor: - add paymentMethod route and server setup               - Added paymentMethod route to handle balance-related requests

---
+++ src/routes/paymentMethod.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import * as paymentMethod from "../controller/paymentMethodController.js";
+
+const app = new Hono();
+
+app.post("/", (c) => paymentMethod.createPaymentMethod(c));
+
+app.get("/:id", (c) => paymentMethod.getPaymentMethodByID(c));
+
+app.get("/:id/payments", (c) => paymentMethod.getPaymentsByPaymentMethodId(c));
+
+app.patch("/:id", (c) => paymentMethod.patchPaymentMethod(c));
+
+app.get("/", (c) => paymentMethod.getAllPaymentMethods(c));
+
+app.post("/:id/expire", (c) => paymentMethod.expirePaymentMethod(c));
+
+app.post("/:id/auth", (c) => paymentMethod.authPaymentMethod(c));
+
+app.post("/:id/simulate", (c) => paymentMethod.simulatePayment(c));
+
+export default app;
